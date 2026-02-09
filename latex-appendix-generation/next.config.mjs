8de52ac79fca1822290749b5ba9044816dbccabd
CS-Won <seokwon0106@yonsei.ac.kr>
2026-02-09 00:38:38 -0800

    Update next.config.mjs

--- latex-appendix-generation/next.config.mjs
+++ latex-appendix-generation/next.config.mjs
@@ -1,11 +1,20 @@
 /** @type {import('next').NextConfig} */
+const repo = "latex-appendix-generation"
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
   },
+
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
 }
 
 export default nextConfig
